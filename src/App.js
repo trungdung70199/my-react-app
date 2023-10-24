e7f4b57f9f001cfaf498bb5b8fd7b8ea007ae158
trungdung70199 <trungdung70199@gmail.com>
2023-10-24 10:15:05 +0900

    up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello react
         </a>
       </header>
     </div>
