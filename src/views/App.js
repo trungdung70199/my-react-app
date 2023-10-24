3a244714bfca714c7183ff6471ca6545e23ce8f9
trungdung70199 <trungdung70199@gmail.com>
2023-10-24 11:18:39 +0900

    update

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.scss';
-
+import MyComponent from './Example/MyComponent';
+/*
+2 components: class component / function component (function, arrow)
+class component: JSX -> HTML + JS
+*/
 function App() {
+  // Co the write function theo cach const App() => {}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello react
         </p>
         <a
           className="App-link"
@@ -15,8 +20,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello react
+          Learn react
         </a>
+        <MyComponent /> {/*Shortcut type*/}
+        {/* <MyComponent></MyComponent> */} {/* Full type */}
       </header>
     </div>
   );
