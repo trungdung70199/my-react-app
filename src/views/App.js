3b2c4802daf1951b1943116cb1f0342e24d4df59
trungdung70199 <trungdung70199@gmail.com>
2023-11-22 14:58:16 +0900

    add Home Nav App

--- src/views/App.js
+++ src/views/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.scss';
 // import MyComponent from './Example/MyComponent';
-// import Form from './Example/Form';
+import Form from './Example/Form';
 import ListTodo from './Todo/ListTodo';
 // import Product from './Example/product';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Home from './Example/Home';
+import Nav from './Nav/Nav';
+import {
+	BrowserRouter,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom"
 
 function App() {
 	return (
+		<BrowserRouter>
 		<div className="App">
 			<header className="App-header">
+				<Nav />
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>Todo app</p>
-				<ListTodo />
+				
 				{/* <MyComponent />  */}
 				{/*Shortcut type*/}
-				{/* <Form /> */}
 				{/* <MyComponent></MyComponent> */} {/* Full type */}
+				<Switch>
+					{/* Path:  */}
+					<Route path="/" exact>
+						<Home />
+					</Route>
+					<Route path="/todo">
+						<ListTodo />
+					</Route>
+					<Route path="/about">
+						<Form />
+					</Route>
+				</Switch>
 			</header>
 			<ToastContainer
 				position="top-right"
@@ -32,6 +53,7 @@ function App() {
 				theme="colored"
 			/>
 		</div>
+		</BrowserRouter>
 	);
 }
 
---
+++ src/views/Example/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>Hello world from home</div>
+        )
+    }
+}
+
+export default Home;
