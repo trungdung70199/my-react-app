f5af58b8097190fe744414f8a5bd3a8b9d74ae00
trungdung70199 <trungdung70199@gmail.com>
2023-12-04 22:26:38 +0900

    update package nav

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -6,11 +6,22 @@ class Nav extends React.Component {
     render() {
         return (
             <div className="topnav">
+
+                {/* <NavLink to="/" activeClassName="active" exact={true}>
+                    Home
+                </NavLink>
+                <NavLink to="/todo" activeClassName="active">
+                    Todo
+                </NavLink>
+                <NavLink to="/about" activeClassName="active">
+                    About
+                </NavLink> */}
+
                 {/* No reload page */}
                 {/* <Link to="/">Home</Link>
                 <Link to="/todo">Todo</Link>
                 <Link to="/about">About</Link> */}
-                <a class="active" href="/">Home</a>
+                <a class="active" href="/" exact={true}>Home</a>
                 <a href="/todo">Todo</a>
                 <a href="/about">About</a>
             </div>
