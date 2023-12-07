58142398df9d073ff18e3f8be0225db7ae124e3a
trungdung70199 <trungdung70199@gmail.com>
2023-12-07 14:06:36 +0900

    update json home

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
-
+import { withRouter } from "react-router";
 class Home extends React.Component {
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push('/todo')
+        }, 3000)
+    }
     render() {
         console.log(">>>check props: ", this.props)
         return (
@@ -14,4 +20,4 @@ class Home extends React.Component {
     }
 }
 
-export default Home;
+export default withRouter(Home);
