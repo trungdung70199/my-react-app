c64ae101e225697b3e9ca60be4f7511a35d6a2c2
trungdung70199 <trungdung70199@gmail.com>
2023-12-08 15:09:22 +0900

    update home

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -2,11 +2,11 @@ import React from "react";
 // import { withRouter } from "react-router";
 class Home extends React.Component {
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.props.history.push('/todo')
-        }, 3000)
-    }
+    // componentDidMount() {
+    //     setTimeout(() => {
+    //         this.props.history.push('/todo')
+    //     }, 3000)
+    // }
     render() {
         console.log(">>>check props: ", this.props)
         return (
